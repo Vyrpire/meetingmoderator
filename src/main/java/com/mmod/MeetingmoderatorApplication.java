f81a22fc68b4a57b1073fcacf2137760318b4212
InstantCrowd <instantcrowd@gmail.com>
2016-11-27 00:51:02 +0100

    Initial commit

---
+++ src/main/java/com/mmod/MeetingmoderatorApplication.java
@@ -0,0 +1,12 @@
+package com.mmod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingmoderatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingmoderatorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mmod/MeetingmoderatorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mmod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeetingmoderatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
