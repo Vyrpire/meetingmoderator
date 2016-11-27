2ba83c9d03da23b95c3b62e42322e89b7715cd05
InstantCrowd <instantcrowd@gmail.com>
2016-11-27 01:09:11 +0100

    Save

---
+++ src/main/java/com/mmod/Room.java
@@ -0,0 +1,20 @@
+package com.mmod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dimi on 27.11.2016.
+ */
+public class Room {
+    // Room DB
+    int room[];
+
+    //Creates Array on creation
+    public Room(int tables) {
+        int room[] = new int[tables];
+    }
+
+
+
+}
